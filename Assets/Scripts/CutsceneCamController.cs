e5454a2b9fc9c8052b3ee5fb8efd48be0b467ebb
Robin Armstrong-Wood <dlw1crt@bolton.ac.uk>
2021-03-02 04:10:51 +0000

    Pan Cam updated, enemies added (need animating), navmesh bug need fixing
    Further progress can't be added until Navmesh is fixed as all code requires navmesh

---
+++ Assets/Scripts/CutsceneCamController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneCamController : MonoBehaviour
+{
+
+    GameObject playerCamera;
+    Animator camAnim;
+
+    GameObject playerVar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camAnim = GameObject.Find("CutsceneCam").GetComponent<Animator>();
+        playerVar = GameObject.Find("Player");
+        playerVar.GetComponent<PlayerMovement>().enabled = false;
+        playerVar.GetComponent<CameraMovement>().enabled = false;
+        playerCamera = GameObject.Find("Player/Main Camera");
+        playerCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(camAnim.GetCurrentAnimatorStateInfo(0).IsName("CutsceneEnd")){
+            playerVar.GetComponent<PlayerMovement>().enabled = true;
+            playerVar.GetComponent<CameraMovement>().enabled = true;
+            playerCamera.SetActive(true);
+            GameObject cutCam = GameObject.Find("CutsceneCam");
+            cutCam.SetActive(false);
+        }
+    }
+}
